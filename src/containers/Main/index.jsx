39dc222b73b32b11530b65eedf730c48b43397d3
Nunes <GilsonNunes_dev@outlook.com>
2020-12-23 11:47:33 -0300

    adicionado lógica e impotação de componentes no Main

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,7 +1,37 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
+import api from "../../api";
 
 function Main() {
-  return <div></div>;
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("brazil");
+
+  const getCovidData = useCallback((country) => {
+    api.getCountry(country).then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+    getCovidData(country);
+  }, [getCovidData, country]);
+
+  const handleChange = ({ target }) => {
+    const country = target.value;
+    setCountry(country);
+  };
+
+  return (
+    <ContainerStyled>
+      <div className="mb-2">
+        <Panel
+          data={data}
+          updateAt={updateAt}
+          onChange={handleChange}
+          country={country}
+          getCovidData={getCovidData}
+        />
+      </div>
+      <Board data={data} />
+    </ContainerStyled>
+  );
 }
 
 export default memo(Main);
